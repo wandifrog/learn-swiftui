7fc6aa89109f4a1163c86d577857f867a9b2c1c7
Wandi <wandifrog@gmail.com>
2021-03-16 19:11:07 +0700

    Initial Commit

---
+++ Learn Swift UI/Learn_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_Swift_UIApp.swift
+//  Learn Swift UI
+//
+//  Created by Wandi on 16/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
