a7aa3f8aa5268713d3ef4c5ec995891e0e4f0151
Wandi <wandifrog@gmail.com>
2021-04-08 19:28:01 +0700

    chore: update all

---
+++ Learn Swift UI/Views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Learn Swift UI
+//
+//  Created by Wandi on 07/04/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
